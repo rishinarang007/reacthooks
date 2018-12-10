77c12637d719710c33e4654932a250c948dcb755
rishinarang007 <rishinarang007@gmail.com>
2018-12-10 17:25:22 +0530

    hacker news

--- src/index.js
+++ src/index.js
@@ -1,14 +1,15 @@
 import React, { useState } from "react";
 import ReactDOM from "react-dom";
+import Application from "./hacker_news/App";
 // import UseStateApp from "./useState";
 // import LoginForm from "./LoginForm_multipleState";
-import LoginForm from "./LoginForm_singleState";
+// import LoginForm from "./LoginForm_singleState";
 import "./styles.css";
 
 function App() {
   return (
     <div className="App" id="apps">
-      <LoginForm />
+      <Application />
     </div>
   );
 }
